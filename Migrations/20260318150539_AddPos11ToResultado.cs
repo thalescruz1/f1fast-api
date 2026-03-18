2725a9a63fddbf756decdc8eea554749dba2a72b
Thales Cruz <thales@venicetech.com.br>
2026-03-18 12:08:46 -0300

    Migration do 11

---
+++ Migrations/20260318150539_AddPos11ToResultado.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace F1Fast.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddPos11ToResultado : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Pos11Id",
+                table: "Resultados",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Pos11Id",
+                table: "Resultados");
+        }
+    }
+}
